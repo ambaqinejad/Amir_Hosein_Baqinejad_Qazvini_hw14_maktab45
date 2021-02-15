d2042180f7a1cd1a848225f5d85943821e19adac
Amir Hosein Baqinejad <ambaqinejad@gmail.com>
2021-02-15 21:28:05 +0330

    products are ready.

--- ex1/controllers/home.js
+++ ex1/controllers/home.js
@@ -1,8 +1,10 @@
 const path = require('path');
+const shoesData = require(path.join(path.dirname(__dirname), 'models', 'shoes.json'));
 
 module.exports = (req, res, next) => {
     res.render(path.join('pages', 'home.ejs'), {
         active: 'home',
-        title: 'Home'
+        title: 'Home',
+        shoes: shoesData
     })
 }
